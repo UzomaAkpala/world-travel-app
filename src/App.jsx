1e4d91072e2927a51bd711fefddf9bcf5f5a5d71
UzomaAkpala <francisakpala@yahoo.com>
2023-09-25 11:12:43 +0100

    Implementing Main Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>World Wise</div>;
+  return (
+    <div>
+      <h1>Hello Router!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>World Wise</h1>;
+}
+
+export default Home;
