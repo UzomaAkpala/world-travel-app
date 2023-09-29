565d7072133122050548ef03e8a74de0b6e0cfe2
UzomaAkpala <francisakpala@yahoo.com>
2023-09-29 14:04:15 +0100

    Adding Authentication: Implementing Login

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -21,7 +21,7 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
+  name: "Uzoma",
   email: "jack@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
@@ -59,6 +59,7 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was outside AuthProvider");
+  return context;
 }
 
 export { AuthProvider, useAuth };
